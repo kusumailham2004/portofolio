7b9f9ff4d49879f25f35f0c7d1070c5f0884db13
kusumailham2004 <kusumailham2004@gmail.com>
2022-06-27 15:23:27 +0700

    revisi27

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-// const makeNavLinksSmooth = ( ) => {
-//     const navLinks = document.querySelectorAll( 'nav ul li a' );
+const makeNavLinksSmooth = ( ) => {
+    const navLinks = document.querySelectorAll( 'nav ul li a' );
   
-//     for ( let n in navLinks ) {
-//       if ( navLinks.hasOwnProperty( n ) ) {
-//         navLinks[ n ].addEventListener( 'click', e => {
-//           e.preventDefault( );
-//           document.querySelector( navLinks[ n ].hash )
-//             .scrollIntoView( {
-//               behavior: "smooth"
-//             } );
-//         } );
-//       }
-//     }
-//   }
+    for ( let n in navLinks ) {
+      if ( navLinks.hasOwnProperty( n ) ) {
+        navLinks[ n ].addEventListener( 'click', e => {
+          e.preventDefault( );
+          document.querySelector( navLinks[ n ].hash )
+            .scrollIntoView( {
+              behavior: "smooth"
+            } );
+        } );
+      }
+    }
+  }
   
-//   const spyScrolling = ( ) => {
-//     const sections = document.querySelectorAll( '.page-scroll' );
+  const spyScrolling = ( ) => {
+    const sections = document.querySelectorAll( '.page-scroll' );
   
-//     window.onscroll = ( ) => {
-//       const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+    window.onscroll = ( ) => {
+      const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
   
-//       for ( let s in sections )
-//         if ( sections.hasOwnProperty( s ) && sections[ s ].offsetTop <= scrollPos ) {
-//           const id = sections[ s ].id;
-//           document.querySelector( '.active' ).classList.remove( 'active' );
-//           document.querySelector( `a[href*=${ id }]` ).parentNode.classList.add( 'active' );
-//         }
-//     }  
-//   }
-//   makeNavLinksSmooth( );
-//   spyScrolling( );
+      for ( let s in sections )
+        if ( sections.hasOwnProperty( s ) && sections[ s ].offsetTop <= scrollPos ) {
+          const id = sections[ s ].id;
+          document.querySelector( '.active' ).classList.remove( 'active' );
+          document.querySelector( `a[href*=${ id }]` ).parentNode.classList.add( 'active' );
+        }
+    }  
+  }
+  makeNavLinksSmooth( );
+  spyScrolling( );
 
 
 const hamburger = document.querySelector(".hamburger");
